ed3bc50c8f8c21f10ccb483770a763a7d91a71bd
min <lsmstar7@gmail.com>
2026-07-04 15:32:42 +0900

    회원가입 페이지 컴포넌트 및 CSS 추가

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import "./Register.css";
+
+function Register () {
+   return (
+     <section className="r1">
+        <button className="backbutton">←</button>
+        <h1 className="registertitle">회원가입</h1>
+        
+     </section>
+   );
+}
+
+export default Register;
