64f679317de536ad29cdef0b3db3ea9da3ac6e23
min <lsmstar7@gmail.com>
2026-07-04 22:20:38 +0900

    회원가입 ui기초토대 완성

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -62,8 +62,62 @@ function Register () {
              >
              {showPassword ? <FiEye /> : <FiEyeOff />}
              </button>
-            </div>
+           </div>
         </div>
+
+        <div className="genderagebox">
+          <div className="genderbuttons">
+             <button className="genderbutton" type="button">
+                남자
+             </button>
+
+             <button className="genderbutton" type="button">
+                여자
+             </button>
+           </div>
+
+           <div className="agebox">
+             <label className="agelabel">나이</label>
+
+             <input
+              className="ageinput"
+              type="number"
+              placeholder="-"
+             />
+           </div>
+        </div>
+
+        <div className="inputbox">
+          <label className="inputlabel">직업(선택)</label>
+
+          <select className="jobselect">
+             <option value="">직업을 선택해주세요</option>
+             <option value="student">학생</option>
+             <option value="college_student">대학생</option>
+             <option value="graduate_student">대학원생</option>
+             <option value="worker">직장인</option>
+             <option value="teacher">교사</option>
+             <option value="professor">교수</option>
+             <option value="freelancer">프리랜서</option>
+             <option value="self_employed">자영업자</option>
+             <option value="public_official">공무원</option>
+             <option value="unemployed">무직</option>
+             <option value="etc">기타</option>
+          </select>
+        </div>
+
+        <div className="agreebox">
+          <input className="agreecheck" type="checkbox" />
+
+          <p className="agreetext">
+             Loopa의 <button type="button">이용약관</button> 및{" "}
+             <button type="button">개인정보처리방침</button>에 동의합니다.
+          </p>
+        </div>
+
+        <button className="submitbutton" type="button">
+           회원가입 완료
+        </button>
      </section>
    );
 }
