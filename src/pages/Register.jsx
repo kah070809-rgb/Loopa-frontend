42c04a599e08457fe84efa8d752f93e67b562534
min <lsmstar7@gmail.com>
2026-07-04 21:45:12 +0900

    비밀번호&눈 아이콘 추가

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./Register.css";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 function Register () {
+   const [showPassword, setShowPassword] = useState(false);
+
    return (
      <section className="r1">
         <button className="backbutton">←</button>
@@ -21,6 +25,45 @@ function Register () {
               </button>
            </div>
          </div>
+
+         <div className="inputbox">
+            <label className="inputlabel">인증번호</label>
+
+            <input
+            className="textinput"
+            type="text"
+            placeholder="인증번호를 입력해주세요"
+            />
+
+          <div className="codebottom">
+             <p className="codeguide">⊙ 이메일로 받은 인증번호를 입력해주세요.</p>
+
+              <button className="resendbutton" type="button">
+                인증번호 재발송
+              </button>
+           </div>
+        </div>
+
+        <div className="divider"></div>
+           
+        <div className="inputbox">
+           <label className="inputlabel">비밀번호</label>
+           <div className="passwordrow">
+             <input
+              className="passwordinput"
+              type={showPassword ? "text" : "password"}
+              placeholder="비밀번호를 입력해주세요"
+             />
+
+             <button
+              className="eyebutton"
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+             >
+             {showPassword ? <FiEye /> : <FiEyeOff />}
+             </button>
+            </div>
+        </div>
      </section>
    );
 }
