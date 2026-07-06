d3ec0a318c44342b3149bb879175288a79289232
min <lsmstar7@gmail.com>
2026-07-06 15:44:24 +0900

    로그인 화면 초기

---
+++ src/pages/Rogin.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./Rogin.css";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
+function Rogin() {
+    return(
+        <section className="r2">
+            <button className="backbutton" type="button">←</button>
+
+        </section>
+    )
+}
+
+export default Rogin;
