66eac95e1684fa2e09dddd33ea26377a01d8fcf6
ahyun <kah070809@gmail.com>
2026-07-13 03:05:51 +0900

    page 수정 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SurveyCreatePage from './pages/SurveyCreate/SurvveyCreate';
-import MainPage from './pages/Mainpage';
+import MainPage from './pages/MainPage';
 import Mypage from './pages/Mypage';
 import Surveys from './pages/SurveysList';
 import AppLayout from './styles/AppLayout';
